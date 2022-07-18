b0260edb2d31c6950a56fd18b41053797b6f7a08
Mariano Ravinale <mravinale@gmail.com>
2022-07-18 14:22:23 -0300

    added integration test

---
+++ src/users/users.spec.ts
@@ -0,0 +1,24 @@
+import { expect } from 'chai';
+import supertest from 'supertest';
+
+import { app } from "../app";
+const entityName: string = 'user';
+
+describe(`users`, () => {
+
+  const server = supertest(app);
+
+  describe('GET /{id}', () => {
+    it(`should get one: ${entityName}`, async () => {
+      var id = 2;
+      const res = await server.get(`/users/${id}`);
+      expect(res.status).to.equal(200);
+      expect(res.body.id).to.deep.equal(id);
+    });
+
+    it(`should FAIL to get one: ${entityName}`, async () => {
+      const res = await server.get(`/users/15`);
+      expect(res.status).to.satisfy((val: number) => val === 404 || val === 500);
+    });
+  });
+});
